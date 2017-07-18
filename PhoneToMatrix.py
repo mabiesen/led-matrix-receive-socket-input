b2bf68ab3923d7d79ce459f20f78a8e9401c00e8
Matthew Biesen <upandcomming88@gmail.com>
2017-07-18 05:32:05 +0000

    Update PhoneToMatrix.py

--- PhoneToMatrix.py
+++ PhoneToMatrix.py
@@ -1,76 +1,22 @@
-import socket
+import SolidTCP
+#import livematrix
+import time
 import threading
-import livematrix
-
-
-global server
-
-def parse_and_send(mystring):
-    colonsplit = mystring.split(b':')
-    print(colonsplit[1])
-    livematrix.livereading(colonsplit[0],colonsplit[1])
-    
-    
-
-def start_server():
-    bind_ip = "192.168.254.41"
-    bind_port = 8080
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((bind_ip, bind_port))
-    server.listen(5)
-
-            
-
-    print("[*] Listening on %s:%d" % (bind_ip, bind_port))
-
-	#send back strings to client
-	#server_manage_client_input determines what to do with a request.
-	#def reply_to_client(client_socket, request):
-	#	success = "success"
-	#	client_socket.send(success)
-	#	client_socket.close()
-
-
-	#this function called as new thread
-	#receives information, then works on a reply
-    def handle_client(client_socket):
-
-	    #print out what the client sends
-        while True:
-            request = None
-            try:
-                request = client_socket.recv(1024)
-                if request is None:
-                    break
-                print("[*] Received %s" % request)
-                request = request.strip()
-                request = request.rstrip(b'\n')
-                if(request == "clear"):
-                    livematrix.clearMatrix()
-                else:
-                    parse_and_send(request)
-            except(KeyboardInterrupt, SystemExit):
-                livematrix.clearMatrix()
-                server.shutdown(socket.SHUT_RDWR)
-                cleanup_stop_thread()
-                sys.exit()
-
-    
-#Constantly scans for connections to clients.
-#spins off a new thread when contact is made with client
-    while True:
-        
-        client, addr = server.accept()
-            
-        print ("[*] Accepted connection from %s:%d" % (addr[0], addr[1]))
-
-	    #spin up our client thread to handle incoming data
-	    #the thread is declared in the first line and started in the second line
-        client_handler = threading.Thread(target=handle_client, args=(client,))
-        client_handler.start()
 
 
 
-server_thread = threading.Thread(target=start_server)
+# call tcp server through a thread.  This will free the main thread
+# then loop to read from the server
 
+server_thread = threading.Thread(target=SolidTCP.StartTheServer)
 server_thread.start()
+while True:
+    thisreceive = SolidTCP.qp.get()
+    stampsplit = thisreceive.split(";")
+    stamptime = stampsplit[1]
+    #if stampsplit[0] == "clear":
+        #livematrix.clear()
+    #else:
+    #    colonsplit = stampsplit[0].split(b':')
+    #    livematrix.livereading(colonsplit[0],colonsplit[1])    
+    print(stampsplit[0])
